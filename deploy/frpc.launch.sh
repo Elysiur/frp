007a7a355ca8af32bce955d097697669bb5e29b7
Your Name <you@examle.com>
2025-06-07 09:06:01 +0800

    sudo to run

--- deploy/frpc.launch.sh
+++ deploy/frpc.launch.sh
@@ -1,13 +1,16 @@
 LAUNCH_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-docker pull Elysiur/frpc:v0.62.1
+
+docker pull elysiur/frpc:v0.62.1
+
 mkdir -p /etc/frp
-cp -i "$LAUNCH_DIR/frpc.toml" "/etc/frp/frpc.toml"
-docker run \
+cp -i "$LAUNCH_DIR/deploy/frpc.toml" "/etc/frp/frpc.toml"
+
+docker run \  
   --name frpc \
   --restart always \
   --network host \
   -e TZ=Asia/Shanghai \
   -v /etc/frp:/etc/frp \
   -d \
-  frps:v0.62.1 \
+  elysiur/frpc:v0.62.1 \
   -c /etc/frp/frpc.toml
